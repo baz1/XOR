5f348e9f533b984d54e55d05cedade3eb4357148
baz1 <baz1.github@gmail.com>
2015-06-03 23:27:25 +0200

    The first commit wasn't the first, my bad!
    README added, optional second argument file2,
    switching to C extension (since this is plain C).

--- main.c
+++ main.c
@@ -8,23 +8,31 @@ typedef long long int int64;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    if ((argc < 2) || (argc > 3))
     {
-        fprintf(stderr, "Usage: xor file1 file2\n");
+        fprintf(stderr, "Usage: xor file1 [file2]\n");
         return 0;
     }
     FILE *f1 = fopen(argv[1], "r");
     if (!f1)
     {
         perror("Error: Could not open the first file for reading");
+        fprintf(stderr, "Usage: xor file1 [file2]\n");
         return 0;
     }
-    FILE *f2 = fopen(argv[2], "r");
-    if (!f2)
+    FILE *f2;
+    if (argc > 2)
     {
-        perror("Error: Could not open the second file for reading");
-        fclose(f1);
-        return 0;
+        f2 = fopen(argv[2], "r");
+        if (!f2)
+        {
+            perror("Error: Could not open the second file for reading");
+            fprintf(stderr, "Usage: xor file1 [file2]\n");
+            fclose(f1);
+            return 0;
+        }
+    } else {
+        f2 = stdin;
     }
     while (true)
     {
@@ -50,6 +58,7 @@ int main(int argc, char *argv[])
             break;
     }
     fclose(f1);
-    fclose(f2);
+    if (argc > 2)
+        fclose(f2);
     return 0;
 }
